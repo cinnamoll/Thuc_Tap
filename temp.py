eb3396d0b98e7f652ba3356777b626b74c3b707b
cinnamoll <daq.for.work198@gmail.com>
2026-06-29 15:35:23 +0700

    first-commit

---
+++ temp.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+from langgraph.graph import StateGraph, END
+from typing import TypedDict, Annotated, Sequence
+from langchain_core.messages import BaseMessage, SystemMessage, HumanMessage, ToolMessage
+from operator import add as add_messages
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace, HuggingFaceEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_core.tools import tool
